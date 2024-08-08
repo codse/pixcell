c27b2df6fd9bab1033df94f28461283da060f9b8
Hari Lamichhane <onwithhari@gmail.com>
2024-08-08 17:35:09 +0545

    perf: fix re-render on color drag

--- src/components/color-picker.tsx
+++ src/components/color-picker.tsx
@@ -5,11 +5,17 @@ import { cn } from '@/lib/utils';
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { ShortCutInfo } from './shortcut-info';
 import { Input } from './ui/input';
+import { useEffect, useState } from 'react';
 
 export const ColorPicker: React.FC<ColorPickerProps> = ({
   selectedColor,
   onColorChange,
 }) => {
+  const [previewColor, setPreviewColor] = useState(selectedColor);
+  useEffect(() => {
+    setPreviewColor(selectedColor);
+  }, [selectedColor]);
+
   return (
     <ToggleGroup
       type="single"
@@ -49,18 +55,19 @@ export const ColorPicker: React.FC<ColorPickerProps> = ({
       <Tooltip>
         <TooltipTrigger asChild>
           <ToggleGroupItem
-            value={selectedColor}
+            value={previewColor}
             variant="outline"
             className={cn(
               'rounded-sm cursor-pointer relative ms-1 aspect-square transition-all p-0'
             )}
-            style={{ backgroundColor: selectedColor }}
+            style={{ backgroundColor: previewColor }}
           >
             <Input
               type="color"
-              value={selectedColor}
+              value={previewColor}
               className="w-full h-full opacity-0"
-              onChange={(event) =>
+              onChange={(event) => setPreviewColor(event.target.value)}
+              onBlur={(event) =>
                 event.target.value && onColorChange(event.target.value)
               }
             />
