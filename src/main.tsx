561d07b5c47e4889610e6cf4b01e45949440cddb
Hari Lamichhane <onwithhari@gmail.com>
2024-08-08 18:05:08 +0545

    feat: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Pixie from './PixCell';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import { PostHogProvider } from 'posthog-js/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TooltipProvider>
-      <Pixie />
-    </TooltipProvider>
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+        person_profiles: 'identified_only',
+      }}
+    >
+      <TooltipProvider>
+        <Pixie />
+      </TooltipProvider>
+    </PostHogProvider>
   </React.StrictMode>
 );
